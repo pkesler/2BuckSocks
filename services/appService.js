4575c053e122008ca6e109e573dbd5fc06273baf
patrickkesler <rimington4987@gmail.com>
2016-09-22 08:39:08 -0600

    morning add  9/22/16

--- services/appService.js
+++ services/appService.js
@@ -1,3 +1,20 @@
-/**
- * Created by patrickkesler on 9/9/16.
- */
+(function () {
+
+    angular.module('myApp')
+        .service('sockService', sockService);
+        // .filter('capitalize', capitalize);
+
+    function sockService($localStorage) {
+        var self = this;
+
+        self.items = [
+            {
+                "title": "Sock 1",
+                "photo": "url...",
+                "Description": 
+            }
+
+        ]
+    }
+
+})();
